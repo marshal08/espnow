bfdc45954e4d577aa102070e842f86a8fb1f5b19
marshal08 <lubomir.beluch@gmail.com>
2025-07-27 23:13:33 +0200

    Update espnow_gateway.cpp

--- espnow_gateway.cpp
+++ espnow_gateway.cpp
@@ -14,3 +14,9 @@ void ESPNOWGateway::on_data_receive(const uint8_t *mac, const uint8_t *incomingD
   ESP_LOGD("espnow", "📥 Received → Topic: %s | Payload: %s", msg.topic, msg.payload);
   esphome::mqtt::global_mqtt_client->publish(msg.topic, msg.payload);
 }
+esp_now_peer_info_t peerInfo = {
+  .peer_addr = {0xF4, 0x12, 0xFA, 0xCA, 0x5E, 0xC4},
+  .channel = 6,
+  .encrypt = false
+};
+esp_now_add_peer(&peerInfo);

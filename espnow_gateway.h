80012ef0d46a54da6c542cc19f1772a31e744ecc
marshal08 <lubomir.beluch@gmail.com>
2025-07-27 22:47:16 +0200

    Update espnow_gateway.h

--- espnow_gateway.h
+++ espnow_gateway.h
@@ -7,23 +7,9 @@ typedef struct {
   char payload[32];
 } Message;
 
-class ESPNOWGateway : public Component {
+class ESPNOWGateway : public esphome::Component {
  public:
-  void setup() override {
-    WiFi.mode(WIFI_STA);
-    esp_now_init();
-    esp_now_register_recv_cb(on_data_receive);
-  }
-
-  static void on_data_receive(const uint8_t *mac, const uint8_t *incomingData, int len) {
-    Message msg;
-    memcpy(&msg, incomingData, sizeof(msg));
-
-    ESP_LOGD("espnow", "📥 Received → Topic: %s | Payload: %s", msg.topic, msg.payload);
-
-    // Publish to MQTT
-    global_mqtt_client->publish(msg.topic, msg.payload);
-  }
-
-  void loop() override {}
+  void setup() override;
+  void loop() override;
+  static void on_data_receive(const uint8_t *mac, const uint8_t *incomingData, int len);
 };
